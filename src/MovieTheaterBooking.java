9a9e8684dbd4f4e07d4d7c1d7e4fd568e1ed5dba
ViswaMithra16 <88958925+ViswaMithra16@users.noreply.github.com>
2022-04-02 12:24:50 -0700

    Update MovieTheaterBooking.java

--- src/MovieTheaterBooking.java
+++ src/MovieTheaterBooking.java
@@ -1,164 +1,146 @@
 import java.util.*;
-import java.io.*;
 
+class MovieTheaterBooking {
 
-class MovieTheaterBooking
-{
-    final int noOfRows=10;  //Given bounds for theater
-    final int noOfColumns=20;
-    int theaterSeatCapacity=noOfRows*noOfColumns;  // Max theater Seat Capacity
-    String seatmap[][];   //  Array to keep track of who is assigned to which seat
-    int lastFilledSeatIndex[];
-    LinkedHashMap<String,ArrayList<String>> bookings;
+    final int noOfRows = 10;  //Given bounds for theater
+    final int noOfColumns = 20;
+    int theaterSeatCapacity = noOfRows * noOfColumns;  // Max theater Seat Capacity
+    String[][] seatMap;  //  Array to keep track of who is assigned to which seat
+    int lastFilledSeatIndex[]; // To track the last index of seat filled in every row
+    LinkedHashMap<String, ArrayList<String>> bookings;
 
-    MovieTheaterBooking()
-    {
-        seatmap=new String[noOfRows][noOfColumns];
-        lastFilledSeatIndex=new int[]{-4,-4,-4,-4,-4,-4,-4,-4,-4,-4};  // Index of each row where the last seat is booked
-        bookings=new LinkedHashMap<>();
+    MovieTheaterBooking() {
+        seatMap = new String[noOfRows][noOfColumns];
+        lastFilledSeatIndex = new int[]{-4, -4, -4, -4, -4, -4, -4, -4, -4, -4};  // Index of each row where the last seat is booked
+        bookings = new LinkedHashMap<>();
     }
 
-    public LinkedHashMap<String,ArrayList<String>> prebooking_checks(String ReservationId,String ReservationSeatCount, FileWriter fileProcessor)
-    {
+    /**
+     * This method is used to handle the pre-booking checks like negative input and seats capacity
+     *
+     * @param reservationId        This is the first parameter holding ReservationId
+     * @param reservationSeatCount This is the second parameter containing the Requested ReservationSeatCount
+     * @param fileProcessor        This is the third parameter to write the error messages to output file
+     * @return bookings This returns the bookings map with reservationId as Key and allocated list of tickets as values
+     */
 
-        //Checks to handle the negative input seatcount and seats capacity
+    public LinkedHashMap<String, ArrayList<String>> prebooking_checks(String reservationId, String reservationSeatCount, FileWriter fileProcessor) {
+
+        //Checks to handle the negative input seat count and seats capacity
         int i_ReservationSeatCount;
         try {
-            i_ReservationSeatCount = Integer.parseInt(ReservationSeatCount);
+            i_ReservationSeatCount = Integer.parseInt(reservationSeatCount);
             System.out.println(i_ReservationSeatCount + "   " + theaterSeatCapacity);
-        }
-
-        catch(NumberFormatException e) {
-            //System.out.println("The number of seats should be an Integer");
-            ArrayList<String> lt=new ArrayList<>();
+        } catch (NumberFormatException e) {
+            ArrayList<String> lt = new ArrayList<>();
             lt.add("The number of seats should be an Integer.");
-            bookings.put(ReservationId,lt);
-            //fileProcessor.writeToFile("The number of seats should be an Integer.");
+            bookings.put(reservationId, lt);
             return bookings;
         }
-        if(i_ReservationSeatCount<=0)
-        {
-            ArrayList<String> lt=new ArrayList<>();
+        if (i_ReservationSeatCount <= 0) {
+            ArrayList<String> lt = new ArrayList<>();
             lt.add("The number of seats cannot be negative.");
-            bookings.put(ReservationId,lt);
-            //System.out.println("The number of seats should not be Negative or Zero");
+            bookings.put(reservationId, lt);
             return bookings;
         }
 
-        if(i_ReservationSeatCount>theaterSeatCapacity)
-        {
-            ArrayList<String> lt=new ArrayList<>();
+        if (i_ReservationSeatCount > theaterSeatCapacity) {
+            ArrayList<String> lt = new ArrayList<>();
             lt.add("The number of seats requested is greater than Theater Capacity");
-            bookings.put(ReservationId,lt);
+            bookings.put(reservationId, lt);
             fileProcessor.writeToFile(bookings);
-            return bookings;
-            //System.out.println("The number of seats should not be greater than Theater Capacity");
-
+        } else {
+            //Allocate seats as per the reservation request count
+            bookTickets(reservationId, i_ReservationSeatCount);
         }
-        else{
-            //allocate seats as per the reservation request count
-            bookTickets(ReservationId,i_ReservationSeatCount);
-            return bookings;
-        }
-
-        // printTheatreMap();
+        return bookings;
     }
 
-    public void printTheatreMap()
-    {
-        for(int r=noOfRows-1;r>=0;r--)
-        {
-            System.out.print(String.valueOf((char)(r+65))+" :\t");
-            for(int c=0;c<noOfColumns;c++)
-            {
-                System.out.print(seatmap[r][c]+"\t");
+    public void printTheatreMap() {
+        for (int r = noOfRows - 1; r >= 0; r--) {
+            System.out.print(String.valueOf((char) (r + 65)) + " :\t");
+            for (int c = 0; c < noOfColumns; c++) {
+                System.out.print(seatMap[r][c] + "\t");
             }
             System.out.println("");
         }
         System.out.println("-------------------------------------------------------------------- Movie Screen -----------------------------------------------------------");
     }
 
-    public void bookTickets(String ReservationId,int i_ReservationSeatCount)
-    {
+    /**
+     * This method is used to book tickets for the input requested
+     *
+     * @param reservationId          This is the first parameter holding ReservationId
+     * @param i_reservationSeatCount This is the second parameter containing the integer value of  Requested ReservationSeatCount
+     */
+    public void bookTickets(String reservationId, int i_reservationSeatCount) {
 
-        // starting the allocation of seats from top to bottom row and left to right in each row.
-        for(int r=noOfRows-1;r>=0;r--)
-        {
-            // tracks the number of seats empty in each row.
-            int seatsEmpty=noOfColumns-lastFilledSeatIndex[r]-4;
-            if(seatsEmpty>=i_ReservationSeatCount)
-            {
-                fillSeats(ReservationId,i_ReservationSeatCount,r);
+        // Starting the allocation of seats from top to bottom row and left to right in each row.
+        for (int r = noOfRows - 1; r >= 0; r--) {
+            // Tracks the number of seats empty in each row.
+            int seatsEmpty = noOfColumns - lastFilledSeatIndex[r] - 4;
+            if (seatsEmpty >= i_reservationSeatCount) {
+                fillSeats(reservationId, i_reservationSeatCount, r);
                 return;
             }
         }
-        // if the seatsEmpty is less than the no of reservation seat count,
-        //calculate the seats remaining in each row (including the buffer of 3 seats also).
-        // 1. if there are no seats the that particular row, iterate through the next row....
-        // 2. Find out the Seats that are less w.r.t to the input request in that particular row
-        // 3. If lessSeats is equal to 1 then, trying ot allocate the seats in group of 2 and 3
+        /* If the seatsEmpty is less than the no of reservation seat count,
+            calculate the seats remaining in each row (including the buffer of 3 seats also).
+         1. if there are no seats the that particular row, iterate through the next row....
+         2. Find out the Seats that are less w.r.t to the input request in that particular row
+         3. If lessSeats is equal to 1 then, trying ot allocate the seats in group of 2 and 3*/
 
-        int MaxConse= Integer.MIN_VALUE;
-        for(int i=0;i<noOfRows;i++) {
-            MaxConse = Math.max(MaxConse, noOfColumns-lastFilledSeatIndex[i]-4);
+        int maxConse = Integer.MIN_VALUE;
+        for (int i = 0; i < noOfRows; i++) {
+            maxConse = Math.max(maxConse, noOfColumns - lastFilledSeatIndex[i] - 4);
         }
-
-
-        for(int r=noOfRows-1;r>=0;r--)
-        {
-            int seatsRem=noOfColumns-lastFilledSeatIndex[r]-4;
-            if(seatsRem<=0)
-            {
+        for (int r = noOfRows - 1; r >= 0; r--) {
+            int seatsRem = noOfColumns - lastFilledSeatIndex[r] - 4;
+            if (seatsRem <= 0) {
                 continue;
             }
-            int lessSeats=i_ReservationSeatCount-seatsRem;
-            if(lessSeats==1 && MaxConse>=2)
-            {
-                fillSeats(ReservationId, i_ReservationSeatCount - 2, r);
-                bookTickets(ReservationId, 2);  // Should end the loop here
-                break;
+            int lessSeats = i_reservationSeatCount - seatsRem;
+            if (lessSeats == 1 && maxConse >= 2) {
+                fillSeats(reservationId, i_reservationSeatCount - 2, r);
+                bookTickets(reservationId, 2);  // Should end the loop here
+            } else {
+                fillSeats(reservationId, i_reservationSeatCount - lessSeats, r);
+                bookTickets(reservationId, lessSeats);
             }
-            else
-            {
-                fillSeats(ReservationId,i_ReservationSeatCount-lessSeats,r);
-                bookTickets(ReservationId,lessSeats);
-                break;
-            }
-
+            break;
         }
     }
 
-    public void fillSeats(String ReservationId,int i_ReservationSeatCount,int row)
-    {
-        // Find the start and end position where the seats are booked in current row.
-        int startFill=lastFilledSeatIndex[row]+4;
-        int endFill=startFill+i_ReservationSeatCount-1;
-        for(int i=startFill;i<=endFill;i++)
-        {
-            seatmap[row][i]=ReservationId;
+    /**
+     * This method is used to fill seats in a row for the number of seats requested and update the theater capacity
+     *
+     * @param reservationId          This is the first parameter holding ReservationId
+     * @param i_reservationSeatCount This is the second parameter containing the integer value of  Requested ReservationSeatCount
+     * @param row                    This is the third parameter which holds the row value.
+     */
+
+    public void fillSeats(String reservationId, int i_reservationSeatCount, int row) {
+        int startFill = lastFilledSeatIndex[row] + 4;
+        int endFill = startFill + i_reservationSeatCount - 1;
+        for (int i = startFill; i <= endFill; i++) {
+            seatMap[row][i] = reservationId;
             //Checking if seats are already allocated to reservationId, if true adding the new seats to the existing entry.
-            if(bookings.containsKey(ReservationId))
-            {
-                bookings.get(ReservationId).add(String.valueOf((char)(row+65))+String.valueOf(i+1));
-            }
-            else
-            {
+            if (bookings.containsKey(reservationId)) {
+                bookings.get(reservationId).add(String.valueOf((char) (row + 65)) + String.valueOf(i + 1));
+            } else {
                 //If no entry is present, add a new entry to the bookings with reservationId and list of tickets booked.
-                ArrayList<String> lt=new ArrayList<>();
-                lt.add(String.valueOf((char)(row+65))+String.valueOf(i+1));
-                bookings.put(ReservationId,lt);
+                ArrayList<String> lt = new ArrayList<>();
+                lt.add(String.valueOf((char) (row + 65)) + String.valueOf(i + 1));
+                bookings.put(reservationId, lt);
             }
         }
-        lastFilledSeatIndex[row]=endFill; // pointed the last seat booked in that row.
-        int diff = noOfColumns - (endFill+1);
-        // updating the total theater seat capacity
-        //If the remaining seats in the row are greater than or equal to 3 then we can still reserve in that row .
-        if(diff>=3){
-            theaterSeatCapacity=theaterSeatCapacity-i_ReservationSeatCount-3;
-        }else{
-            theaterSeatCapacity=theaterSeatCapacity-i_ReservationSeatCount-diff;
+        lastFilledSeatIndex[row] = endFill; // pointed the last seat booked in that row.
+        int diff = noOfColumns - (endFill + 1);
+        //If the remaining seats in the row are greater than or equal to 3 then we can still reserve in that row. Updating the total theater seat capacity
+        if (diff >= 3) {
+            theaterSeatCapacity = theaterSeatCapacity - i_reservationSeatCount - 3;
+        } else {
+            theaterSeatCapacity = theaterSeatCapacity - i_reservationSeatCount - diff;
         }
-        //theaterSeatCapacity=theaterSeatCapacity-i_ReservationSeatCount;
     }
-
 }
