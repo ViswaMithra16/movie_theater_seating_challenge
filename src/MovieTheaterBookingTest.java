1f84df109e4ecf2179237c5e9f81cc6681fc1f17
ViswaMithra16 <88958925+ViswaMithra16@users.noreply.github.com>
2022-04-02 12:25:12 -0700

    Update MovieTheaterBookingTest.java

--- src/MovieTheaterBookingTest.java
+++ src/MovieTheaterBookingTest.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -20,6 +21,7 @@ public class MovieTheaterBookingTest {
             Assert.assertEquals(e.getMessage(), "File Name should be valid");
         }
     }
+
     @Test
     public void bookingTestCase() {
         LinkedHashMap<String, ArrayList<String>> response = new LinkedHashMap<>();
@@ -36,6 +38,7 @@ public class MovieTheaterBookingTest {
             Assert.assertEquals(e.getMessage(), "File Name should be valid");
         }
     }
+
     @Test
     public void bookingMoreThanCapacity() {
         LinkedHashMap<String, ArrayList<String>> response = new LinkedHashMap<>();
@@ -48,8 +51,29 @@ public class MovieTheaterBookingTest {
             System.out.println("Response--> " + response.get("R00A"));
             Assert.assertEquals(response.get("R00A"), list);
         } catch (Exception e) {
-
             Assert.assertEquals(e.getMessage(), "File Name should be valid");
         }
     }
+
+
+    @Test
+    public void bookingSeatsWithNegativeCount() {
+        LinkedHashMap<String, ArrayList<String>> response = new LinkedHashMap<>();
+        FileWriter fileProcessor = new FileWriter();
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> actual = new ArrayList<>();
+        actual.add("The number of seats cannot be negative.");
+        list.add("-1");
+        MovieTheaterBooking theater = new MovieTheaterBooking();
+        try {
+            response = theater.prebooking_checks("R001", "-1", fileProcessor);
+            System.out.println("Response--> " + response.get("R001"));
+            Assert.assertEquals(response.get("R001"), actual);
+        } catch (Exception e) {
+            Assert.assertEquals(e.getMessage(), "------------------ ");
+        }
+    }
+
+
+
 }
